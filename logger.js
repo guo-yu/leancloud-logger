bea73b6eefbe0e018601dcd5ac94cd68752da1e8
Guo Yu <i@guoyu.me>
2014-11-05 16:53:56 +0800

    0.1: It works

--- logger.js
+++ logger.js
@@ -3,9 +3,15 @@ var logger = require("morgan");
 var express = require('express');
 var compress = require('compression');
 var bodyParser = require('body-parser');
+var AV = require('avoscloud-sdk').AV;
+var Log = AV.Object.extend("log");
 
+var configs = require('./configs');
 var pkg = require('./package.json');
-var debug = require('debug')(pkg.name + ':v' + pkg.version);
+var debug = require('debug')(pkg.name);
+
+// Init LeanCloud instance 
+AV.initialize(configs.appId, configs.appKey);
 
 module.exports = logger();
 
@@ -25,8 +31,37 @@ function logger() {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
+  // Logger API
+  app.post('/', log);
+
   // Start
   app.listen(app.get('port'));
 
   debug('Started, NODE_ENV=%s, PORT=%s', app.get('env'), app.get('port'));
+}
+
+function log(req, res, next) {
+  var token = req.headers['logger-token'];
+  var logDetails = req.body;
+
+  if (!token || !logDetails || token !== )
+    return res.send('fail');
+
+  var baby = new Log();
+
+  baby.save(
+    logDetails, 
+    configs.successCallback || successCallback, 
+    configs.errorCallback || errorCallback
+  );
+
+  function successCallback(data) {
+    
+  }
+
+  function errorCallback(data, err) {
+    debug('error');
+    debug(err);
+    debug('error end');
+  }
 }
